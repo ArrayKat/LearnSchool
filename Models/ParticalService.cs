281b9ce18db1dd956e73846fa154809d14696e3c
unknown <@ngknn.local>
2024-12-11 11:18:18 +0300

    feat: add sort, filter, administrator mode

--- Models/ParticalService.cs
+++ Models/ParticalService.cs
@@ -15,6 +15,6 @@ namespace LernSchool.Models
         public int DurationInMinutes => DurationInSeconds / 60;
 
         public int DiscountInt => (int)(Discount !=null ? Discount * 100 : 0);
-
+        public string BackgroundColor => Discount == null ? "#FFFFFF" : "#E7FABF";
     }
 }
--- ViewModels/ShowServiceVM.cs
+++ ViewModels/ShowServiceVM.cs
@@ -12,11 +12,23 @@ namespace LernSchool.ViewModels
 {
     internal class ShowServiceVM : ViewModelBase
     {
-        public List<Service> ListServices => MainWindowViewModel.myConnection.Services.ToList();
+        List<Service> _listServices;
 
-        public async void DeleteService(int idService) {
+        public List<Service> ListServices { get => _listServices; set => this.RaiseAndSetIfChanged(ref _listServices, value); }
+
+
+        public ShowServiceVM()
+        {
+            ListServices = MainWindowViewModel.myConnection.Services.ToList();
+        }
+
+
+        #region Change, delete
+        public async void DeleteService(int idService)
+        {
             ButtonResult result = await MessageBoxManager.GetMessageBoxStandard("Сообщение", "Хотите удалить данную услугу?", ButtonEnum.YesNo).ShowAsync();
-            if (result == ButtonResult.Yes) {
+            if (result == ButtonResult.Yes)
+            {
                 Service deleteService = MainWindowViewModel.myConnection.Services.FirstOrDefault(s => s.Id == idService);
                 List<ClientService> serviceClient = MainWindowViewModel.myConnection.ClientServices.Where(x => x.ServiceId == idService).ToList();
                 //если на услугу никто не записан - список пустой, занчит можно удалять
@@ -28,12 +40,64 @@ namespace LernSchool.ViewModels
                     MainWindowViewModel.Instance.PageContent = new ShowService();
                 }
             }
+
+        }
+
+        public void ChangeService(int id)
+        {
+            MainWindowViewModel.Instance.PageContent = new AddChangeServices(id);
+        }
+        #endregion
+
+        string _searchNameDesc = "";
+        int _sortCostIndex = 0;
+        int _filterDiscountIndex = 0;
+        public string SearchNameDesc { get => _searchNameDesc; set { this.RaiseAndSetIfChanged(ref _searchNameDesc, value); AllFilters(); } }
+        public int SortCostIndex { get => _sortCostIndex; set { _sortCostIndex = value; AllFilters(); } }
+        public int FilterDiscountIndex { get => _filterDiscountIndex; set { this.RaiseAndSetIfChanged(ref _filterDiscountIndex, value); AllFilters(); } }
+
+        
+
+        void AllFilters()
+        {
+            //обновляем список сервисов
+            //ListServices = MainWindowViewModel.myConnection.Services.ToList();
+            ListServices.Clear();
+            ListServices = MainWindowViewModel.myConnection.Services.ToList();
+            //для поиска по имени и описанию (сработает, если поле ввода не пустое)
+            if (!string.IsNullOrEmpty(_searchNameDesc))
+            {
+                //Выбираем все сервисы, в которых название или описание (маленькими буквами) содержит строчку введенную пользователем
+                ListServices = ListServices.Where(x => x.Title.ToLower().Contains(_searchNameDesc.ToLower()) || x.Description.ToLower().Contains(_searchNameDesc.ToLower())).ToList();
+            }
+
+            switch (_sortCostIndex)
+            {
+                case 1: ListServices = ListServices.OrderBy(x => x.CostResult).ToList(); break;
+                case 2: ListServices = ListServices.OrderByDescending(x => x.CostResult).ToList(); break;
+                default: ListServices = ListServices.ToList(); break;
+            }
+
+            switch (_filterDiscountIndex)
+            {
+                case 1: ListServices = ListServices.Where(x => x.DiscountInt >= 0 && x.DiscountInt < 5).ToList(); break;
+                case 2: ListServices = ListServices.Where(x => x.DiscountInt >= 5 && x.DiscountInt < 15).ToList(); break;
+                case 3: ListServices = ListServices.Where(x => x.DiscountInt >= 15 && x.DiscountInt < 30).ToList(); break;
+                case 4: ListServices = ListServices.Where(x => x.DiscountInt >= 30 && x.DiscountInt < 70).ToList(); break;
+                case 5: ListServices = ListServices.Where(x => x.DiscountInt >= 70 && x.DiscountInt < 100).ToList(); break;
+                default: ListServices = ListServices.ToList(); break;
+            }
             
         }
 
-        public void ChangeService(int id) {
-            MainWindowViewModel.Instance.PageContent = new AddChangeServices(id);
-        }
-        
+        string _adminCode = "";
+        bool _isVisibleAdmin = false;
+        public string AdminCode { get => _adminCode; set { this.RaiseAndSetIfChanged(ref _adminCode, value); } }
+
+        public bool IsVisibleAdmin { get => _isVisibleAdmin; set => this.RaiseAndSetIfChanged(ref _isVisibleAdmin, value); }
+
+        //public bool CheckAdmin => Convert.ToInt32(_adminCode)==000000 ? false : true;
+        public void CheckAdmin(){ IsVisibleAdmin = AdminCode == "000000" ? true : false; }
+
     }
 }
